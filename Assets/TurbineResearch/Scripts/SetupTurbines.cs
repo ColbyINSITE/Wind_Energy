2f699db553e467c8eb35ccc2ea0b9e7747879cba
ngoclinhp842 <83108702+ngoclinhp842@users.noreply.github.com>
2022-11-09 17:23:44 -0500

    Update SetupTurbines.cs

--- Assets/TurbineResearch/Scripts/SetupTurbines.cs
+++ Assets/TurbineResearch/Scripts/SetupTurbines.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class SetupTurbines : MonoBehaviour
 {
     /*
@@ -23,6 +24,7 @@ public class SetupTurbines : MonoBehaviour
     private float tz;
 
     // Start is called before the first frame update
+    /*
     void Start()
     {   
         tx = transform.position.x;
@@ -55,4 +57,5 @@ public class SetupTurbines : MonoBehaviour
         }
         return positions;
     }
+    */
 }
