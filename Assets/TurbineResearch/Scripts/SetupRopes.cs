c8361da16e21208d5697bb65a5f9c8a19176f5be
Izge Bayyurt <griezwahlm@gmail.com>
2024-07-08 16:27:44 -0400

    Getting the rope to render properly
    To do:
    - Set material in the code
    - Fix stretching issue

--- Assets/TurbineResearch/Scripts/SetupRopes.cs
+++ Assets/TurbineResearch/Scripts/SetupRopes.cs
@@ -19,20 +19,20 @@ public class SetupRopes : MonoBehaviour
         CreateRope(); 
     }
 
-    IEnumerator SetupBlueprint(ObiRopeBlueprint blueprint)
+    void SetupBlueprint(ObiRopeBlueprint blueprint)
     {
         // Procedurally generate the rope path (a simple straight line):
         int filter = ObiUtils.MakeFilter(ObiUtils.CollideWithEverything, 0);
         blueprint.path.Clear();
-        for (int i = 0; i < 6; i += 3)
+        int count = 1;
+        for (int i = 0; i < table[0].Count; i += 3)
         {
-            blueprint.path.AddControlPoint(new Vector3(table[0][i],table[0][i+2],table[0][i+1]), -Vector3.right, 
-                Vector3.right, Vector3.up, 0.1f, 0.1f, 1, filter, Color.white, "control");
+            Vector3 position = new Vector3(table[0][i], table[0][i + 2], table[0][i + 1]);
+            blueprint.path.AddControlPoint(position, Vector3.zero, 
+                Vector3.zero, Vector3.up, 0.1f, 0.1f, 1, filter, Color.white, "control" + count);
+            count++;
         }
         blueprint.path.FlushEvents();
-
-        // generate the particle representation of the rope (wait until it has finished):
-        yield return StartCoroutine(blueprint.Generate());
     }
 
     ObiSolver CreateSolver()
@@ -62,9 +62,10 @@ public class SetupRopes : MonoBehaviour
 
         ObiRopeBlueprint blueprint = ScriptableObject.CreateInstance<ObiRopeBlueprint>();
 
-        IEnumerator setupBlueprint = SetupBlueprint(blueprint);
+        SetupBlueprint(blueprint);
+        IEnumerator bpSetup = blueprint.Generate();
 
-        while (setupBlueprint.MoveNext()) {}
+        while (bpSetup.MoveNext()) {}
         
         // instantiate and set the blueprint:
         rope.ropeBlueprint = ScriptableObject.Instantiate(blueprint);
@@ -73,30 +74,27 @@ public class SetupRopes : MonoBehaviour
         
         // parent the cloth under a solver to start simulation:
         rope.transform.parent = solver.transform;
+
+        int groupIndex = 0;
+        for (int i = 0; i < table[0].Count; i += 3)
+        {
+            Vector3 position = new Vector3(table[0][i], table[0][i + 2], table[0][i + 1]);
+            AddAttachment(ropeObject, CreateEmptyGameObject(position), rope.blueprint.groups[groupIndex]);
+            groupIndex += 1;
+        }
     }
 
-    void AddConstraints(ObiRope rope)
+    Transform CreateEmptyGameObject(Vector3 position)
     {
-        // get a hold of the constraint type we want, in this case, pin constraints:
-        var pinConstraints = rope.GetConstraintsByType(Oni.ConstraintType.Pin) as ObiConstraints<ObiPinConstraintsBatch>;
-
-        // remove all batches from it, so we start clean:
-        pinConstraints.Clear();
-
-        // create a new pin constraints batch
-        var batch = new ObiPinConstraintsBatch();
-
-        // Add a couple constraints to it, pinning the first and last particles in the rope:
-        // batch.AddConstraint(rope.solverIndices[0], colliderA, Vector3.zero, Quaternion.identity, 0, 0, float.PositiveInfinity);
-        // batch.AddConstraint(rope.solverIndices[blueprint.activeParticleCount - 1], colliderB, Vector3.zero, Quaternion.identity, 0, 0, float.PositiveInfinity);
-
-        // set the amount of active constraints in the batch to 2 (the ones we just added).
-        batch.activeConstraintCount = 2;
-
-        // append the batch to the pin constraints:
-        pinConstraints.AddBatch(batch);
-
-        // this will cause the solver to rebuild pin constraints at the beginning of the next frame:
-        rope.SetConstraintsDirty(Oni.ConstraintType.Pin);
+        GameObject go = new GameObject();
+        go.transform.position = position;
+        return go.transform;
+    }
+    
+    void AddAttachment(GameObject actor, Transform targetTransform, ObiParticleGroup particleGroup)
+    {
+        var attachment = actor.AddComponent<ObiParticleAttachment>();
+        attachment.target = targetTransform;
+        attachment.particleGroup = particleGroup;
     }
 }
