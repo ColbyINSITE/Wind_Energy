c846c17c5c773bc1d163b83bc54559205616ce7e
Izge Bayyurt <griezwahlm@gmail.com>
2024-07-09 17:32:03 -0400

    Created rope animations
    Setup two coroutines that call each other to animate the ropes. The timestep of the animation is 0.005f per frame, but this could be altered to Time.deltaTime.
    
    Todo:
    
    Document/comment the code
    Add support for multiple ropes

--- Assets/TurbineResearch/Scripts/SetupRopes.cs
+++ Assets/TurbineResearch/Scripts/SetupRopes.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Obi;
@@ -8,30 +9,78 @@ public class SetupRopes : MonoBehaviour
 {
     private CSVReader csvReader;
     private List<List<float>> table;
-
+    private int nodeCount = 0;
+    
+    
+    public List<TextAsset> csvFiles; 
     public Material ropeMat;
     
     
-    void Start()
+    void Awake()
     {
+        string path = "C:\\Users\\IMRE Lab\\Wind_Energy\\Assets\\TurbineResearch\\CSV Files\\Output1.csv";
+        csvFiles = new List<TextAsset>();
         csvReader = new CSVReader();
-        table = csvReader.ReadCSVFile(
-            "C:\\Users\\IMRE Lab\\Wind_Energy\\Assets\\TurbineResearch\\CSV Files\\Output1.csv", 2);
+        table = csvReader.ReadCSVFile(Application.dataPath + "\\TurbineResearch\\CSV Files\\Output1.csv", 2);
         CreateRope(); 
     }
 
-    void SetupBlueprint(ObiRopeBlueprint blueprint)
+    private void Start()
+    {
+        AnimateRopes();
+    }
+
+    private void AnimateRopes()
+    {
+        for (int i = 0; i < nodeCount; i++)
+        {
+            Transform node = transform.GetChild(i);
+            StartCoroutine(LoopNodes(node, i));
+        }
+    }
+
+    IEnumerator LoopNodes(Transform node, int nodeIdx)
+    {
+        for (int i = 1; i < 1200; i++)
+        {
+            IEnumerator lerpNode = LerpNode(node, nodeIdx, i);
+            while (lerpNode.MoveNext())
+            {
+                yield return null;
+            }
+        }
+    }
+
+    IEnumerator LerpNode(Transform node, int nodeIdx, int frame)
+    {
+        Vector3 startPosition = node.position;
+        Vector3 endPosition = new Vector3(table[frame][3 * nodeIdx], table[frame][3 * nodeIdx + 2], table[frame][3 * nodeIdx + 1]);
+        float timeStep = 0f;
+        float lerpDuration = 0.5f;
+        while (timeStep < lerpDuration)
+        {
+            node.position = Vector3.Lerp(startPosition, endPosition, timeStep/lerpDuration);
+            timeStep += 0.005f;
+            yield return null;
+        }
+
+        node.position = endPosition;
+
+    }
+
+    void SetupBlueprint(ObiRodBlueprint blueprint)
     {
         // Procedurally generate the rope path (a simple straight line):
         int filter = ObiUtils.MakeFilter(ObiUtils.CollideWithEverything, 0);
         blueprint.path.Clear();
-        int count = 1;
+
         for (int i = 0; i < table[0].Count; i += 3)
         {
+            nodeCount += 1;
             Vector3 position = new Vector3(table[0][i], table[0][i + 2], table[0][i + 1]);
             blueprint.path.AddControlPoint(position, Vector3.zero, 
-                Vector3.zero, Vector3.up, 0.1f, 0.1f, 1, filter, Color.white, "control" + count);
-            count++;
+                Vector3.zero, Vector3.up, 0.1f, 0.1f, 1, filter, Color.white, "control" + nodeCount);
+
         }
         blueprint.path.FlushEvents();
     }
@@ -53,10 +102,10 @@ public class SetupRopes : MonoBehaviour
     void CreateRope()
     {
         // create a rope:
-        GameObject ropeObject = new GameObject("rope", typeof(ObiRope), typeof(ObiRopeExtrudedRenderer));
+        GameObject ropeObject = new GameObject("rope", typeof(ObiRod), typeof(ObiRopeExtrudedRenderer));
 
         // get component references:
-        ObiRope rope = ropeObject.GetComponent<ObiRope>();
+        ObiRod rope = ropeObject.GetComponent<ObiRod>();
         ObiRopeExtrudedRenderer ropeRenderer = ropeObject.GetComponent<ObiRopeExtrudedRenderer>();
 
         // load the default rope section:
@@ -66,7 +115,8 @@ public class SetupRopes : MonoBehaviour
         ropeObject.GetComponent<MeshRenderer>().material = ropeMat;
         
         // create a blueprint 
-        ObiRopeBlueprint blueprint = ScriptableObject.CreateInstance<ObiRopeBlueprint>();
+        ObiRodBlueprint blueprint = ScriptableObject.CreateInstance<ObiRodBlueprint>();
+        blueprint.resolution = 0.01f;
 
         SetupBlueprint(blueprint);
         IEnumerator bpSetup = blueprint.Generate();
@@ -74,7 +124,7 @@ public class SetupRopes : MonoBehaviour
         while (bpSetup.MoveNext()) {}
         
         // instantiate and set the blueprint:
-        rope.ropeBlueprint = ScriptableObject.Instantiate(blueprint);
+        rope.rodBlueprint = ScriptableObject.Instantiate(blueprint);
 
         ObiSolver solver = CreateSolver();
         
@@ -94,6 +144,7 @@ public class SetupRopes : MonoBehaviour
     {
         GameObject go = new GameObject();
         go.transform.position = position;
+        go.transform.parent = transform;
         return go.transform;
     }
     
